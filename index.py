a64f47316d1296eb9b7c974ffcaa48e3bbfcf1d8
Ben Shaughnessy <bs7280@gmail.com>
2017-04-21 20:26:41 -0500

    Can get page rank, did not do much additional testing, added one test file based on power point, does not ouput to file, almost done

--- index.py
+++ index.py
@@ -3,7 +3,8 @@ import re
 import os
 import collections
 import time
-#import other modules as needed
+import numpy as np
+from scipy.linalg import eig
 
 class pagerank:
 
@@ -34,29 +35,44 @@ class pagerank:
         for i in range(0,len(matrix)):
             row_total = float(sum(matrix[i]))
 
-        print("Raw Probability Matrix")
-        print(matrix)
-
         # Modifying matrix to
         for i in range(0,len(matrix)):
             row_total = sum(matrix[i])
-            if row_total == 0:
-                for j in range(0,len(matrix[i])):
-                    matrix[i][j] = 1.0/float(numbPages)
-            else:
-                for j in range(0,len(matrix[0])):
-                    matrix[i][j] = (1 - teleportation_rate)*float(matrix[i][j]/row_total) + teleportation_rate/float(numbPages)
+            #if row_total == 0:
+            #    for j in range(0,len(matrix[i])):
+            #        matrix[i][j] = (1 - teleportation_rate)*(1.0/float(numbPages)) + teleportation_rate/float(numbPages)
+            #else:
+            for j in range(0,len(matrix[0])):
+                matrix[i][j] = (1.0 - teleportation_rate)*float(matrix[i][j]/row_total) + teleportation_rate/float(numbPages)
 
-        # Testing
-        for i in range(0,len(matrix)):
-            #print(matrix[i])
-            assert(abs(sum(matrix[i]) - 1) < 0.001)
+        # Get initial matrix P
+        P = np.full((numbPages, 1), 1.0/float(numbPages))
+        P = np.matrix(P)
+
+        # Get left hand eigenvector of transition probability matrix as A
+        w, vl, vr = eig(np.matrix(matrix), left=True)
+        A = vl
+
+        A = np.matrix(matrix).transpose()
+
+        i = 0
+        while i < 100:
+            P = A*P
+            i+=1
+
+        # Sort the final result by page rank and get top ten results
+        final = list(zip(list(map(lambda x: round(x[0],3), P.tolist())),range(0,numbPages)))
+        final.sort(reverse=True, key=lambda x: x[0])
+        final = final[:10]
 
 
-        print(matrix)
+        for i,x in enumerate(final):
+            print(str(i)+".","Doc Number", x[1]," -  Rank:", x[0], "")
+
+
 
 
 
 
 pr = pagerank()
-pr.pagerank("test1.txt")
+pr.pagerank("test3.txt")
